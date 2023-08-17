5e2c2e12239f0617f3e182d3409b22a644316348
elsamrafisptr <134671598+elsamrafisptr@users.noreply.github.com>
2023-08-17 21:04:45 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Hamburger menu
+
+const hamburger = document.querySelector('#hamburger');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active')
+});
